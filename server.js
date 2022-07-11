f0b415ca99cba63ae79aa70c92758e3a6fed9d87
Tovape <tonivalverde0@gmail.com>
2022-07-11 19:15:32 +0200

    reset password

--- server.js
+++ server.js
@@ -22,6 +22,21 @@ initializePassport(
   id_user => users.find(user => user.id_user === id_user)
 )
 
+app.set('view-engine', 'ejs')
+app.use(busboy());
+app.use(express.static(path.join(__dirname)));
+app.use(favicon(__dirname + '/files/icons/favicon.ico'));
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
 // SQL Connection
 
 const db = mysql.createConnection({
@@ -31,6 +46,8 @@ const db = mysql.createConnection({
 	password: '123',
 	database: "reflow"
 });
+
+// Get Users
 
 const users = [];
 
@@ -50,22 +67,46 @@ db.query(query, function (err, result, fields) {
 	}
 });
 
-app.set('view-engine', 'ejs')
-app.use(busboy());
-app.use(express.static(path.join(__dirname)));
-app.use(favicon(__dirname + '/files/icons/favicon.ico'));
-app.use(express.urlencoded({ extended: false }))
-app.use(flash())
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
+// Get User Requests Function
 
-// Main Pages
+let getRequests = async (user_id) => {
+	
+    const query = "SELECT * FROM requests WHERE id_user = " + user_id;
+	let requests = [];
+	let result = await new Promise((resolve, reject) => db.query(query, (err, result) => {
+		if (err) {
+			reject(err)
+		} else {
+			resolve(result);
+			console.log("\nGot Requests")
+			for(let i = 0; i < result.length; i++) {
+				result[i] = JSON.parse(JSON.stringify(result[i]));
+				requests.push(result[i]);
+			}
+		}
+	}));
+	console.log(requests)
+	return requests;
+}
+
+// Auth Function
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/dashboard')
+  }
+  next()
+}
+
+// GET
+
 app.get('/', function (req, res) {
 	res.render('index.ejs'),
 	app.use(express.static(__dirname + '/css')),
@@ -115,19 +156,12 @@ app.get('/success', function (req, res) {
 	app.use(express.static(__dirname + '/js'))
 })
 
-// Signup Login
 app.get('/login', checkNotAuthenticated, (req, res) => {
 	res.render('login.ejs'),
 	app.use(express.static(__dirname + '/css')),
 	app.use(express.static(__dirname + '/files')),
 	app.use(express.static(__dirname + '/js'))
 })
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/dashboard',
-  failureRedirect: '/login',
-  failureFlash: true
-}))
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
 	res.render('register.ejs'),
@@ -136,9 +170,48 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
 	app.use(express.static(__dirname + '/js'))
 })
 
+app.get('/dashboard', checkAuthenticated, async (req, res) => {
+	let user_id = req.user[Object.keys(req.user)[0]];
+	let requests = await getRequests(user_id);
+
+	console.log("\nLogged as Username: " + req.user[Object.keys(req.user)[1]] + " | Email: " + req.user[Object.keys(req.user)[2]] + " | Id: " + req.user[Object.keys(req.user)[0]]);
+	res.render('dashboard.ejs', { data: req.user, requests: requests }),
+	app.use(express.static(__dirname + '/css')),
+	app.use(express.static(__dirname + '/files')),
+	app.use(express.static(__dirname + '/js'))
+})
+
+app.get('/editor', checkAuthenticated, (req, res) => {
+	res.render('editor.ejs'),
+	app.use(express.static(__dirname + '/css')),
+	app.use(express.static(__dirname + '/files')),
+	app.use(express.static(__dirname + '/js'))
+})
+
+app.get('/resetpassword', checkNotAuthenticated, (req, res) => {
+	res.render('resetpassword.ejs'),
+	app.use(express.static(__dirname + '/css')),
+	app.use(express.static(__dirname + '/files')),
+	app.use(express.static(__dirname + '/js'))
+})
+
+app.get('/newpassword', function (req, res) {
+	res.render('newpassword.ejs'),
+	app.use(express.static(__dirname + '/css')),
+	app.use(express.static(__dirname + '/files')),
+	app.use(express.static(__dirname + '/js'))
+})
+
+// POST
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
 app.post('/register', checkNotAuthenticated, async (req, res) => {
 
-	// Password Validation
 	var passwordExpression  = /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 	var emailExpression = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
@@ -179,62 +252,6 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
 	}
 })
 
-app.delete('/logout', (req, res) => {
-	req.logout(req.user, err => {
-		if(err) return next(err);
-		res.redirect("/login");
-	});
-});
-
-// Password Reset
-app.get('/passwordreset', checkNotAuthenticated, (req, res) => {
-	res.render('passwordreset.ejs'),
-	app.use(express.static(__dirname + '/css')),
-	app.use(express.static(__dirname + '/files')),
-	app.use(express.static(__dirname + '/js'))
-})
-
-
-
-// Get User Requests Function
-let getRequests = async (user_id) => {
-	
-    const query = "SELECT * FROM requests WHERE id_user = " + user_id;
-	let requests = [];
-	let result = await new Promise((resolve, reject) => db.query(query, (err, result) => {
-		if (err) {
-			reject(err)
-		} else {
-			resolve(result);
-			console.log("\nGot Requests")
-			for(let i = 0; i < result.length; i++) {
-				result[i] = JSON.parse(JSON.stringify(result[i]));
-				requests.push(result[i]);
-			}
-		}
-	}));
-	console.log(requests)
-	return requests;
-}
-
-app.get('/dashboard', checkAuthenticated, async (req, res) => {
-	let user_id = req.user[Object.keys(req.user)[0]];
-	let requests = await getRequests(user_id);
-
-	console.log("\nLogged as Username: " + req.user[Object.keys(req.user)[1]] + " | Email: " + req.user[Object.keys(req.user)[2]] + " | Id: " + req.user[Object.keys(req.user)[0]]);
-	res.render('dashboard.ejs', { data: req.user, requests: requests }),
-	app.use(express.static(__dirname + '/css')),
-	app.use(express.static(__dirname + '/files')),
-	app.use(express.static(__dirname + '/js'))
-})
-
-app.get('/editor', checkAuthenticated, (req, res) => {
-	res.render('editor.ejs'),
-	app.use(express.static(__dirname + '/css')),
-	app.use(express.static(__dirname + '/files')),
-	app.use(express.static(__dirname + '/js'))
-})
-
 app.post('/editor', checkAuthenticated, (req, res) => {
 	let user_id = req.user[Object.keys(req.user)[0]];
 	console.log("Redirecting to editor ejs with request " + req.body.request_id + " from " + user_id)
@@ -244,22 +261,18 @@ app.post('/editor', checkAuthenticated, (req, res) => {
 	app.use(express.static(__dirname + '/js'))
 })
 
-// Auth Functions
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect('/login')
-}
+// DELETE
 
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/dashboard')
-  }
-  next()
-}
+app.delete('/logout', (req, res) => {
+	req.logout(req.user, err => {
+		if(err) return next(err);
+		res.redirect("/login");
+	});
+});
 
-// Get Avatar Image
+
+// ROUTE
+
 app.route('/saveavatar').post(function (req, res, next) {
 	var fstream;
 	req.pipe(req.busboy);
@@ -275,7 +288,6 @@ app.route('/saveavatar').post(function (req, res, next) {
 	});
 });
 
-// Send Contact/Booking Form Mail
 app.route('/contactform').post(function (req, res, next) {
 
 	var transporter = nodemailer.createTransport({
@@ -365,7 +377,7 @@ app.route('/bookingform').post(function (req, res, next) {
 });
 
 // Password Resetting
-app.route('/passwordreset').post(function (req, res, next) {
+app.route('/resetpassword').post(function (req, res, next) {
 
 	// Create Encrypted ID
 	var encrypted = CryptoJS.AES.encrypt(req.body.email, "Tovape");
@@ -399,13 +411,6 @@ app.route('/passwordreset').post(function (req, res, next) {
 	});
 });
 
-app.get('/newpassword', function (req, res) {
-	res.render('newpassword.ejs'),
-	app.use(express.static(__dirname + '/css')),
-	app.use(express.static(__dirname + '/files')),
-	app.use(express.static(__dirname + '/js'))
-})
-
 app.route('/newpassword').post(async function (req, res, next) {
 	
 	// Get Email
@@ -433,5 +438,6 @@ app.route('/newpassword').post(async function (req, res, next) {
 });
 
 // Port
+
 console.log("Server Started on port " + PORT);
 app.listen(PORT);
